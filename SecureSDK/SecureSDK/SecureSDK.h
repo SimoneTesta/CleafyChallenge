775de5918eaa9cfd4b4829d2d0d40ee4a78b21ed
Simone Testa <sim.testa97@gmail.com>
2022-02-12 13:41:36 +0100

    project setup

---
+++ SecureSDK/SecureSDK/SecureSDK.h
@@ -0,0 +1,18 @@
+//
+//  SecureSDK.h
+//  SecureSDK
+//
+//  Created by Simone Testa on 12/02/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SecureSDK.
+FOUNDATION_EXPORT double SecureSDKVersionNumber;
+
+//! Project version string for SecureSDK.
+FOUNDATION_EXPORT const unsigned char SecureSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SecureSDK/PublicHeader.h>
+
+
